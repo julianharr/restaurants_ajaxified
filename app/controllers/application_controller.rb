06e28a45d116dfbbd749b28ec529c96b1cd9d4ab
Julian Harrington <jrayharrington@gmail.com>
2021-02-12 13:47:15 +1100

    added restaurant counter on home page and restored route for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # Before ANY action on any controller, do this:
+  before_action :set_counter
+
+  private
+
+  def set_counter
+    @restaurant_count = Restaurant.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   resources :restaurants, only: [ :index, :show ] do
     # Only need somewhere to create the form then we can redirect somewhere else
     # Have to nest it so that we know what restaurant we're talking about
